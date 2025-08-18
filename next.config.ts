fe56bddf6509fdb2d192d7b61dc2a5b83ba07f4d
wiley <weixiong.zhang@xatali.com>
2025-08-18 16:54:38 +0800

    移除

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from 'next';
 
 
 const isProduction = process.env.NODE_ENV === 'production';
-const repositoryName = 'test-blog'; // <--- 替换成你的仓库名！
+const repositoryName = ''; // <--- 替换成你的仓库名！
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProduction ? `/${repositoryName}` : '',
-  assetPrefix: isProduction ? `/${repositoryName}/` : '',
+  basePath: '',
+  assetPrefix: '',
     images: {
     unoptimized: true,
   },
